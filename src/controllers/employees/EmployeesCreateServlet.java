9bb1a756a16a2b6d62f88659cfab676fa6e82ee4
K.Marker <s.kodama.scrap@gmail.com>
2021-03-02 21:07:53 +0900

    Add login / logout

--- src/controllers/employees/EmployeesCreateServlet.java
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -45,7 +45,8 @@ public class EmployeesCreateServlet extends HttpServlet {
             e.setCode(request.getParameter("code"));
             e.setName(request.getParameter("name"));
             e.setPassword(
-                    EncryptUtil.getPasswordEncrypt("password",
+                    EncryptUtil.getPasswordEncrypt(
+                            request.getParameter("password"),
                             (String)this.getServletContext().getAttribute("pepper")
                             )
                     );
